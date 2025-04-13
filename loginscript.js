054ed922fe58f8d6d0e4355c59ad54240bd605d5
Vivek Raj <vivekrajspj2019@gmail.com>
2025-04-13 15:31:47 +0530

    Added show and hide password button in the login page along with the link to the vault

--- loginscript.js
+++ loginscript.js
@@ -9,3 +9,43 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 }) 
+const password1= document.querySelector("#password1");
+const password2= document.querySelector("#password2");
+
+var shbutton = document.querySelector(".shpass");
+var shbutton1 = document.querySelector("#shpass1");
+var shbutton2 = document.querySelector("#shpass2");
+
+
+
+function shpassf1(){
+    if(password1.type=="password" ){
+        password1.type="text";
+        shbutton1.textContent="Hide Password";
+        shbutton2.textContent="Hide Password";
+
+    }
+    else{
+        password1.type="password";
+        shbutton1.textContent="Show Password";
+        shbutton2.textContent="Show Password";
+
+    }
+
+}
+
+function shpassf2(){
+    if(password2.type=="password" ){
+        password2.type="text";
+        shbutton1.textContent="Hide Password";
+        shbutton2.textContent="Hide Password";
+
+    }
+    else{
+        password2.type="password";
+        shbutton1.textContent="Show Password";
+        shbutton2.textContent="Show Password";
+
+    }
+
+}
